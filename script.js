16b96585dddb2f469c5642687b77f40be844b630
Jamie Flynt <jamflynt@gmail.com>
2021-11-07 21:33:29 -0500

    Fixed function, removed .innerText.

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ async function randAdvice(){
         console.log(`Random Advice GET successful`);
         const randAdvice = await axios.get(`https://api.adviceslip.com/advice`);
         console.log(randAdvice.data);
-        const div1 = document.querySelector(`.advice`);
-        div1.innerText = randAdvice.data.slip.advice;
+        console.log(randAdvice.data.slip.advice); 
 
     } catch (err){
         console.log(`Random Advice GET failed`);
