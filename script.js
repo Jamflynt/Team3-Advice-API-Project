47666717d40d0f557ddc51796b8f87ed52f889e6
Jamie Flynt <jamflynt@gmail.com>
2021-11-14 13:32:51 -0500

    Added lucky numbers  to fortune div. Fixed some styling.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const adviceLoc = document.querySelector(`.fortune-text`)
 
+// Random piece of advice
 async function randAdvice(){
     try {
         console.log(`Random Advice GET successful`);
@@ -13,19 +14,38 @@ async function randAdvice(){
         console.log(err);
     }
 }
-// randAdvice();
 
 const fortuneCookie = document.querySelector(`.fortune-cookie`)
 const fortuneRight = document.querySelector(`.right`)
 const fortuneLeft = document.querySelector(`.left`)
 
+const luckyNum = document.querySelector(`.lucky-numbers`)
 
+
+const ranNum1 = Math.floor(Math.random()*100)+1;
+const ranNum2 = Math.floor(Math.random()*100)+1;
+const ranNum3 = Math.floor(Math.random()*100)+1;
+const ranNum4 = Math.floor(Math.random()*100)+1;
+const yourNums = `Your lucky numbers: ${ranNum1}, ${ranNum2}, ${ranNum3}, ${ranNum4}`;
+
+function yourLuckyNums(){
+    setTimeout(() => {
+        luckyNum.innerText = yourNums;
+    }, 440)
+}
+
+
+
+// Run event listener
 fortuneCookie.addEventListener(`click`, () => {
     fortuneRight.classList.add(`right-open`);
     fortuneLeft.classList.add(`left-open`);
     randAdvice();
-    fortuneCookie.removeEventListener(`click`);
+    yourLuckyNums();
 })
+
+
+
 
 const reset = document.querySelector(`button`)
 
@@ -33,4 +53,6 @@ reset.addEventListener(`click`, () => {
     fortuneRight.classList.remove(`right-open`);
     fortuneLeft.classList.remove(`left-open`);
     adviceLoc.innerText = "";
+    luckyNum.innerText = "";
 })
+
